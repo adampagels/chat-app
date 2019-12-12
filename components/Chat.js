26d3f5389ed438e9b2d8f400481b25bc15849bfd
adampagels <50380598+adampagels@users.noreply.github.com>
2019-12-12 14:20:22 -0500

    Add 5.7.2

--- components/Chat.js
+++ components/Chat.js
@@ -66,7 +66,7 @@ export default class Chat extends Component {
     // go through each document
     querySnapshot.forEach(doc => {
       // get the QueryDocumentSnapshot's data
-      var data = doc.data();
+      let data = doc.data();
       messages.push({
         _id: data._id,
         text: data.text,
